46ad85c508b1722634e297b23a68062aa07dda22
Xi Chen <xichen6@microsoft.com>
2023-01-19 12:32:01 +0800

    when the view model inherit the INotifyPropertyChanged interface, the notification should do by developer itself. We should trigger PropertyChanged event when the value is changed, then ui will update,

--- BuildYourOwnObservableObject/BuildYourOwnObservableObject/ViewModels/UserInfoViewModel.cs
+++ BuildYourOwnObservableObject/BuildYourOwnObservableObject/ViewModels/UserInfoViewModel.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace BuildYourOwnObservableObject.ViewModels
 {
-    internal class UserInfoViewModel/* : INotifyPropertyChanged*/
+    internal class UserInfoViewModel : INotifyPropertyChanged
     {
         private string _firstName;
         private string _lastName;
