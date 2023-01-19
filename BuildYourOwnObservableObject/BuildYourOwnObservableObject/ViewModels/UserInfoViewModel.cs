a04747c992c3f5e7e0a24e5b70d48be5bbb44455
Xi Chen <xichen6@microsoft.com>
2023-01-19 12:03:55 +0800

    without implementing inotifypropertychanged, the property changes will not trigger ui changing

---
+++ BuildYourOwnObservableObject/BuildYourOwnObservableObject/ViewModels/UserInfoViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BuildYourOwnObservableObject.ViewModels
+{
+    internal class UserInfoViewModel
+    {
+        public string FirstName { get; set; }
+
+        public UserInfoViewModel()
+        {
+            Task.Run(() => { 
+                while(true)
+                {
+                    FirstName = new Random().Next(1, 1000).ToString();
+                    Debug.WriteLine($"FirstName: {FirstName}");
+                    Thread.Sleep(500);
+                }
+            });
+        }
+    }
+}
