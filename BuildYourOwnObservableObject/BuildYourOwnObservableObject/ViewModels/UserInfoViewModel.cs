4203fd0afbc7be4f1a0445d87ce76550b9fe24b0
Xi Chen <xichen6@microsoft.com>
2023-01-19 12:14:44 +0800

    an naive implementation of inotifypropertychanged

--- BuildYourOwnObservableObject/BuildYourOwnObservableObject/ViewModels/UserInfoViewModel.cs
+++ BuildYourOwnObservableObject/BuildYourOwnObservableObject/ViewModels/UserInfoViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Diagnostics;
+using System.IO.Pipes;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -8,20 +10,40 @@ using System.Threading.Tasks;
 
 namespace BuildYourOwnObservableObject.ViewModels
 {
-    internal class UserInfoViewModel
+    internal class UserInfoViewModel : INotifyPropertyChanged
     {
-        public string FirstName { get; set; }
+        private string _firstName;
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set
+            {
+                _firstName = value;
+                OnPropertyChanged(this, new PropertyChangedEventArgs(nameof(FirstName)));
+            }
+        }
 
         public UserInfoViewModel()
         {
-            Task.Run(() => { 
-                while(true)
+            Task.Run(() =>
+            {
+                while (true)
                 {
                     FirstName = new Random().Next(1, 1000).ToString();
                     Debug.WriteLine($"FirstName: {FirstName}");
                     Thread.Sleep(500);
                 }
             });
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void OnPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged.Invoke(sender, e);
+            }
         }
     }
 }
