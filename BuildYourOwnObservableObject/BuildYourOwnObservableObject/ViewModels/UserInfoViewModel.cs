a3db136a13b4e08531ce0d5ffe2ca3b407cf619c
Xi Chen <xichen6@microsoft.com>
2023-01-19 12:30:10 +0800

    without inherit inotifypropertychanged, there is a wpf supported feature that we can change ui from ui.

--- BuildYourOwnObservableObject/BuildYourOwnObservableObject/ViewModels/UserInfoViewModel.cs
+++ BuildYourOwnObservableObject/BuildYourOwnObservableObject/ViewModels/UserInfoViewModel.cs
@@ -10,28 +10,55 @@ using System.Threading.Tasks;
 
 namespace BuildYourOwnObservableObject.ViewModels
 {
-    internal class UserInfoViewModel : INotifyPropertyChanged
+    internal class UserInfoViewModel/* : INotifyPropertyChanged*/
     {
         private string _firstName;
+        private string _lastName;
 
+
+        /*
         public string FirstName
         {
             get { return _firstName; }
             set
             {
                 _firstName = value;
-                OnPropertyChanged(this, new PropertyChangedEventArgs(nameof(FirstName)));
+                //OnPropertyChanged(this, new PropertyChangedEventArgs(nameof(FirstName)));
+                //OnPropertyChanged(this, new PropertyChangedEventArgs(nameof(FullName)));
+            }
+        }
+        */
+
+        public string FirstName { get; set; }
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set
+            {
+                _lastName = value;
+                //OnPropertyChanged(this, new PropertyChangedEventArgs(nameof(LastName)));
+                //OnPropertyChanged(this, new PropertyChangedEventArgs(nameof(FullName)));
             }
         }
 
+        public string FullName
+        {
+            get
+            {
+                return $"{FirstName} {LastName}";
+            }
+        }
         public UserInfoViewModel()
         {
             Task.Run(() =>
             {
                 while (true)
                 {
-                    FirstName = new Random().Next(1, 1000).ToString();
+                    //FirstName = new Random().Next(1, 1000).ToString();
                     Debug.WriteLine($"FirstName: {FirstName}");
+                    Debug.WriteLine($"LastName: {LastName}");
+                    Debug.WriteLine($"FullName: {FullName}");
                     Thread.Sleep(500);
                 }
             });
